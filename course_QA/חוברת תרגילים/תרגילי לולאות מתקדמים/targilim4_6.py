e2886dbdc065412b1ffe475ffdd460bbe28fd329
johnathanQAcourse <johnathannun@gmail.com>
2021-12-11 17:30:47 +0200

    Targilim that we've done

--- course_QA/חוברת תרגילים/תרגילי לולאות מתקדמים/targilim4_6.py
+++ course_QA/חוברת תרגילים/תרגילי לולאות מתקדמים/targilim4_6.py
@@ -1,8 +1,9 @@
-num = int(input("enter number"))
-k = []
-while num > 10:
-    print(num%10)
-    k.append(num%10)
-    num = num // 10
-for i in range k(len(k)):
-print(2*int(i))
+# num = input("enter number")
+# print(num[::-1])
+# print(2*int(num[::-1]))
+
+num = input("enter number")
+rev = ''
+for i in range(len(num), 0, -1):
+    rev += num[i-1]
+print(int(rev))
